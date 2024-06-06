d5d78e07823e69c25bf40e141efbcb6e6a952434
Sonny95 <thswlgml9598@gmail.com>
2024-06-07 09:49:22 +1000

    created analogue,digital components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./css/App.css";
-import "./css/clocks.css";
 import { useState, useEffect } from "react";
+import AnalogueClock from "../src/components/analogue";
+import DigitalClock from "../src/components/digital";
 
 function App() {
   //get current time
@@ -18,53 +19,13 @@ function App() {
     return { hour, minute, second };
   };
 
-  //digi time
-  const [time, setTime] = useState(getTime());
-
-  //update every second
-  useEffect(() => {
-    const interval = setInterval(() => {
-      //save time to setTime
-      setTime(getTime());
-    }, 1000);
-    //clear interval before renew second
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    //get current time function
-    const { hour, minute, second } = getTime();
-    //hour moves 30degree every hour + 30/60 = 0.5
-    const degHour = hour * (360 / 12) + minute * (360 / 12 / 60);
-    const degMinute = minute * (360 / 60);
-    const degSecond = second * (360 / 60);
-
-    //javascript given variable name and can be used for css
-    const hourHand = document.querySelector(".hour");
-    const minuteHand = document.querySelector(".minute");
-    const secondHand = document.querySelector(".second");
-
-    //spin them each respective degree
-    if (hourHand && minuteHand && secondHand) {
-      hourHand.style.transform = `rotate(${degHour}deg)`;
-      minuteHand.style.transform = `rotate(${degMinute}deg)`;
-      secondHand.style.transform = `rotate(${degSecond}deg)`;
-    }
-  }, [getTime()]);
-
   return (
     <div className="App">
       <header className="App-header">
         {/* analogueClock */}
-        <div className="analogueContainer">
-          <div className="clock">
-            <div className="hour"></div>
-            <div className="minute"></div>
-            <div className="second"></div>
-          </div>
-        </div>
+        <AnalogueClock getTime={getTime} />
         {/* digitalClock */}
-        <div className="digitalContainer">{`${time.hour}:${time.minute}:${time.second}`}</div>
+        <DigitalClock getTime={getTime} />
       </header>
     </div>
   );
